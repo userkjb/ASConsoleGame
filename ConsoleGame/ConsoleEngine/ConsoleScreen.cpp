33c5af601054212940761a7659099a9daeef7cc0
kjb <kjb_jh@naver.com>
2024-01-04 15:21:37 +0900

    Add Engine is Test Function
    # 엔진에 테스트용 함수를 추가.

--- ConsoleGame/ConsoleEngine/ConsoleScreen.cpp
+++ ConsoleGame/ConsoleEngine/ConsoleScreen.cpp
@@ -1,6 +1,12 @@
 #include "ConsoleScreen.h"
+#include <iostream>
 
 void ConsoleScreen::Render(const char* _Char)
 {
+	int Size = static_cast<int>(strlen(_Char));
 
+	for (int i = 0; i < Size; i++)
+	{
+		printf_s("%c", _Char[i]);
+	}
 }
