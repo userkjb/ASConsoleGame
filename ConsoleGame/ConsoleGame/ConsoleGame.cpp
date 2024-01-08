f28e355f556d9b2413e75808c66f312e0462427c
kjb <kjb_jh@naver.com>
2024-01-08 16:47:59 +0900

    Create Bullet in Main
    # 메인에서 불릿 생성.

--- ConsoleGame/ConsoleGame/ConsoleGame.cpp
+++ ConsoleGame/ConsoleGame/ConsoleGame.cpp
@@ -3,6 +3,7 @@
 #include <ConsoleEngine/EngineDebug.h>
 
 #include "Player.h"
+#include "Bullet.h"
 
 int main()
 {
@@ -14,11 +15,27 @@ int main()
 	Player NewPlayer;
 	NewPlayer.SetPos({ Screen.GetScreenX() / 2, Screen.GetScreenY() - 2});
 
+	const int BulletCount = Screen.GetScreenY() * 2;
+	Bullet* NewBullet = new Bullet[BulletCount];
+	int CurBullet = 0;
+
 	while (true)
 	{
 		Sleep(100);
 
 		NewPlayer.InputKey();
+
+		if (NewPlayer.GetIsFire() == true)
+		{
+			NewBullet[CurBullet].SetPos(NewPlayer.GetPos());
+			NewBullet[CurBullet].Fire();
+			CurBullet++;
+
+			if (BulletCount <= CurBullet)
+			{
+				CurBullet = 0;
+			}
+		}
 
 		Screen.SetChar(NewPlayer);
 		Screen.PrintScreen();
