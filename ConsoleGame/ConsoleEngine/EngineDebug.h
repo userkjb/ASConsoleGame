a5faab70e8ecfb4cff50ae0466f56da3fde65268
kjb <kjb_jh@naver.com>
2024-01-08 12:25:16 +0900

    Update EngineDebug class
    # EngineDebug 클래스에서 클래스를 뺀 다음,
    # LeckCheck 용 Define과
    # MsgBosAssert Define 추가.

--- ConsoleGame/ConsoleEngine/EngineDebug.h
+++ ConsoleGame/ConsoleEngine/EngineDebug.h
@@ -1,5 +1,7 @@
 #pragma once
-class EngineDebug
-{
-};
 
+#include <cassert>
+
+#define LeckCheck _CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
+
+#define MsgBoxAssert(TEXT) MessageBoxA(nullptr, TEXT, "치명적 에러", MB_OK); assert(false);
