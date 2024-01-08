291bff6bcd42deed765344e3ea76ebeb4518ccf0
kjb <kjb_jh@naver.com>
2024-01-08 16:10:31 +0900

    Add Keyboard get value is Switch
    # 키보드 입력을 받는 리턴 함수 추가. -> _getch()
    # 입력받은 키의 값대로 나뉘는 Switch문 추가.

--- ConsoleGame/ConsoleGame/Player.cpp
+++ ConsoleGame/ConsoleGame/Player.cpp
@@ -1,1 +1,27 @@
 #include "Player.h"
+#include <conio.h>
+
+void Player::InputKey()
+{
+	IsFire = false;
+
+	int Select = _getch();
+
+	switch (Select)
+	{
+	case 'W':
+	case 'w':
+		break;
+	case 'A':
+	case 'a':
+		break;
+	case 'S':
+	case 's':
+		break;
+	case 'D':
+	case 'd':
+		break;
+	default :
+		break;
+	}
+}
