3a6f5a66b4e779dc2f173f28a52d5923ebbb393c
kjb <kjb_jh@naver.com>
2024-01-16 14:54:22 +0900

    Add Reneder Release
    # 렌더 릴리즈 기능 구현

--- ConsoleGame/ConsoleEngine/ConsoleCore.cpp
+++ ConsoleGame/ConsoleEngine/ConsoleCore.cpp
@@ -64,8 +64,42 @@ void ConsoleCore::Start()
 		Screen.PrintScreen();
 		// End
 
+		// 릴리즈 //
+		
+		// 렌더 릴리즈
+		{
+			// 렌더니까 Order 사용.
+			std::map<int, std::list<ConsoleObject*>>::iterator OrderStartIter = AllRenderObject.begin();
+			std::map<int, std::list<ConsoleObject*>>::iterator OrderEndIter = AllRenderObject.end();
+		
+			for (; OrderStartIter != OrderEndIter; ++OrderStartIter)
+			{
+				std::list<ConsoleObject*>& ObjectList = OrderStartIter->second;
 
-	}// end while
+				std::list<ConsoleObject*>::iterator StartIter = ObjectList.begin();
+				std::list<ConsoleObject*>::iterator EndIter = ObjectList.end();
+				for (; StartIter != EndIter; )
+				{
+					ConsoleObject* Object = *StartIter;
+
+					if (false == Object->IsPendingKill())
+					{
+						++StartIter;
+						continue;
+					}
+
+					if (nullptr == Object)
+					{
+						MsgBoxAssert("오브젝트가 nullptr인 경우가 존재합니다.");
+					}
+
+					// 노드를 지운것.
+					StartIter = ObjectList.erase(StartIter);
+				}
+			}
+		} // end 렌더 릴리즈
+
+	} // end while
 }
 
 void ConsoleCore::EngineEnd()
